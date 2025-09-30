1eef4150f9ac7de1cf422395fb51e83c74e36f73
KUANG-JIAN-AI <k025c1007@g.neec.ac.jp>
2025-09-30 13:29:59 +0900

    修复时间戳转日期格式

--- app/models/user_mod.py
+++ app/models/user_mod.py
@@ -14,8 +14,8 @@ class Users(db.Model):
         return {
             "id": self.id,
             "nickname": self.nickname,
-            "created_at": self.created_at.strftime("%Y:%M:%D") if self.created_at else None,
-            "updated_at": self.updated_at.strftime("%Y:%M:%D") if self.updated_at else None
+            "created_at": datetime.fromtimestamp(self.created_at).strftime("%Y-%m-%d %H:%M:%S") if self.created_at else None,
+            "updated_at": datetime.fromtimestamp(self.updated_at).strftime("%Y-%m-%d") if self.updated_at else None
         }
 
     def __repr__(self):
